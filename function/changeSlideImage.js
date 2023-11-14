feff91423b6b541b5f0bd27e906377d6da6e25ce
ki3nAMK <123nguyenbahoangkien123@gmail.com>
2023-11-15 01:58:38 +0700

    fixVariantBugAndAddFeature

--- function/changeSlideImage.js
+++ function/changeSlideImage.js
@@ -1,10 +1,8 @@
 const imgList = document.querySelectorAll(".selectImg") ;
 const imgReceiver = document.querySelector(".imgReceiver");
 
-console.log(imgList[0].src)
 imgList.forEach( elem => {
     elem.addEventListener("click", (e) => {
-        console.log(e)
         imgReceiver.src=elem.src;
     } )
 })
